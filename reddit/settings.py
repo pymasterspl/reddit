03752b180f14f30a565b1da164831b30d175bfb9
Jacek Ostański <jacek@ivolution.pl>
2024-05-04 16:22:42 +0200

    readme

--- reddit/settings.py
+++ reddit/settings.py
@@ -9,7 +9,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import json
 from pathlib import Path
+
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,12 +22,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-e4pma3)-c!52=l7+#68cj_qg(nx)o*s1q439+mwan6%+&srldd" # noqa:S105
+
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = []
+DEBUG = config("DEBUG")
+
+
+# SECURITY WARNING: don't run with debug turned on in production!
+
+
+ALLOWED_HOSTS = json.loads(config("ALLOWED_HOSTS"))
 
 
 # Application definition
