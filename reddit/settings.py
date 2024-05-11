f41f4a69d9d2d81c71fc8fcbc26acfc608487ce8
pawel <pawel@bartczak.tech>
2024-05-11 17:18:48 +0100

    Added custom user model with customer user manager
    This overwrites default django behaviour and allows to create users based on email instead of username

--- reddit/settings.py
+++ reddit/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "users",
 ]
 
 MIDDLEWARE = [
@@ -129,3 +130,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "users.User"
--- users/models.py
+++ users/models.py
@@ -1,3 +1,41 @@
-# from django.db import models #noqa: ERA001
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractUser
+from django.db import models
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):  # noqa: ANN202, ANN101, ANN001, ANN003
+        if not email:
+            message = "Users must have an email address"
+            raise ValueError(message)
+        email: str = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):  # noqa: ANN201, ANN101, ANN001, ANN003, D102
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):  # noqa: ANN201, ANN001, ANN003, ANN101, D102
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")  # noqa: EM101, TRY003
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")  # noqa: EM101, TRY003
+
+        return self._create_user(email, password, **extra_fields)
+
+
+class User(AbstractUser):
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []  # noqa: RUF012
+    objects = UserManager()
+    username = None
+    email = models.EmailField(unique=True)
