7535a8ac2a38cdfad20b220c966bd08dfd584652
Pawelooo <paweloo0147@gmail.com>
2024-05-12 17:17:46 +0200

    Correct test

--- articles/tests/test_base_page.py
+++ articles/tests/test_base_page.py
@@ -1,10 +1,11 @@
+import os
+
+import django
 import pytest
 from django.test import Client
 from django.urls import reverse
-import os
-import django
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'reddit.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "reddit.settings")
 django.setup()
 
 @pytest.mark.django_db()
