28572942a1c2228ea19b628e97bc23bd5938ff74
Pawelooo <paweloo0147@gmail.com>
2024-05-12 15:57:48 +0200

    New view for privacy police

--- articles/views.py
+++ articles/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def home_view(request: HttpRequest) -> HttpRequest:
     return render(request, "base.html")
+
+def privacy_policy(request: HttpRequest) -> HttpRequest:
+    return render(request, "privacy-police.html")
