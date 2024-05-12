82eac6b88a026bd9791cd4932d5c7f39ff74e682
Pawelooo <paweloo0147@gmail.com>
2024-05-12 17:16:56 +0200

    Correct path

--- articles/tests/test_base_page.py
+++ articles/tests/test_base_page.py
@@ -1,7 +1,11 @@
 import pytest
 from django.test import Client
 from django.urls import reverse
+import os
+import django
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'reddit.settings')
+django.setup()
 
 @pytest.mark.django_db()
 def test_privacy_police_view() -> None:
