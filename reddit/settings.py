1762b46d54d3e96bcc0aaa31d2bbde68ee38d29e
Pawelooo <paweloo0147@gmail.com>
2024-05-12 14:06:41 +0200

    Add static_files dirs

--- reddit/settings.py
+++ reddit/settings.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 import json
+import os
 from pathlib import Path
 
 from decouple import config
@@ -62,7 +63,7 @@ ROOT_URLCONF = "reddit.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, 'templates')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -124,6 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
