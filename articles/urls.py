78f8729fd5f8f068fdae02ed878ad7490fa3ab46
Pawelooo <paweloo0147@gmail.com>
2024-05-12 15:48:49 +0200

    Correct code

--- articles/urls.py
+++ articles/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
-from articles.views import HomeView
+from articles.views import home_view
 
 urlpatterns = [
-        path("", HomeView.as_view(), name="home-page"),
+        path("", home_view, name="home-page"),
 ]
