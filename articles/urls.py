d1a17d52be44679b9b48718c34a15a1d3f941bed
Pawelooo <paweloo0147@gmail.com>
2024-05-12 15:57:27 +0200

    New url to privacy police

--- articles/urls.py
+++ articles/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from articles.views import home_view
+from articles.views import home_view, privacy_policy
 
 urlpatterns = [
         path("", home_view, name="home-page"),
+        path('privacy-police', privacy_policy, name='privacy-policy')
 ]
