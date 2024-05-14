7b656e67132e12454cf7e35b84def906c1006fbc
pawel <pawel@bartczak.tech>
2024-05-14 10:41:36 +0100

    Added user model to Django Admin
    This enables viewing users in Admin Panel and protects password from accidental change.

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,28 @@
-# from django.contrib import admin #noqa: ERA001
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
 
-# Register your models here.
+from users.models import User
+
+FieldsetsType = tuple[tuple[None, dict[str, str | tuple[str]]]]
+
+
+@admin.register(User)
+class CustomUserAdmin(DjangoUserAdmin):
+    list_display: list[str] = DjangoUserAdmin.list_display[1:]
+    ordering: tuple[str] = ("email",)
+    fieldsets: FieldsetsType = ()
+    add_fieldsets: FieldsetsType = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "first_name",
+                    "last_name",
+                    "password1",
+                    "password2",
+                ),
+            },
+        ),
+    )
