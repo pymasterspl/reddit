8c67fec8553e62c9780fa6f9d2e4fce9a0809d5b
Pawelooo <paweloo0147@gmail.com>
2024-05-16 16:37:07 +0200

    Correct code

--- articles/tests/test_privacy_police.py
+++ articles/tests/test_privacy_police.py
@@ -1,12 +1,12 @@
 import pytest
-from django.urls import reverse
 from django.test import Client
+from django.urls import reverse
 
 
-@pytest.mark.django_db
-def test_privacy_police_view():
+@pytest.mark.django_db()
+def test_privacy_police_view() -> None:
     client = Client()
-    url = reverse('privacy-policy')
+    url = reverse("privacy-policy")
     response = client.get(url)
-    assert response.status_code == 200
+    assert response.status_code == "200"
     assert "privacy-police.html" in [t.name for t in response.templates]
