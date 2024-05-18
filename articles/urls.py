5569242be8840c3b792e7ff6a73542d4ede8222f
Pawelooo <paweloo0147@gmail.com>
2024-05-18 15:31:13 +0200

    Change address

--- articles/urls.py
+++ articles/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from articles.views import HomeView, PrivacyPoliceView
+from articles.views import HomeView, PrivacyPolicyView
 
 urlpatterns = [
         path("", HomeView.as_view(), name="home-page"),
-        path("privacy-police/", PrivacyPoliceView.as_view(),
+        path("privacy-policy/", PrivacyPolicyView.as_view(),
              name="privacy-policy"),
 
 ]
