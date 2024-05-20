185924ed7597dba0d07b17df02e2132067ad2b70
Krzysztof Knop <knop.krzysiek@gmail.com>
2024-05-20 17:41:27 +0200

    [feature] move constants from tests to conftest

---
+++ conftest.py
@@ -0,0 +1,32 @@
+import secrets
+import string
+
+import pytest
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+def generate_random_password(length: int = 12) -> str:
+    characters = string.ascii_letters + string.digits + string.punctuation
+    secure_random = secrets.SystemRandom()
+    return "".join(secure_random.choice(characters) for _ in range(length))
+
+
+@pytest.fixture()
+def generated_password() -> str:
+    return generate_random_password()
+
+
+@pytest.fixture()
+def user(generated_password: str) -> User:
+    user = User.objects.create_user(email="test@example.com", password=generated_password)
+    user.plain_password = generated_password
+    return user
+
+
+@pytest.fixture()
+def admin_user(generated_password: str) -> User:
+    admin_user = User.objects.create_superuser(email="admin@example.com", password=generated_password)
+    admin_user.plain_password = generated_password
+    return admin_user
