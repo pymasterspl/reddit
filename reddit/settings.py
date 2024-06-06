9af5164995a93ff162826fbfd39262598050e336
Pawelooo <paweloo0147@gmail.com>
2024-06-06 17:44:39 +0200

    Add email settings

--- reddit/settings.py
+++ reddit/settings.py
@@ -148,3 +148,10 @@ MEDIA_URL = "/media/"
 # http://django-crispy-forms.readthedocs.io/en/latest/install.html#template-packs
 CRISPY_TEMPLATE_PACK = "bootstrap5"
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
