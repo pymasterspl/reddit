7b6b6468efe866af467ff15cadb7d0b6eabbb8f8
Pawelooo <paweloo0147@gmail.com>
2024-06-06 23:12:50 +0200

    Add default value in config

--- reddit/settings.py
+++ reddit/settings.py
@@ -153,5 +153,5 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_HOST_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = config("EMAIL_HOST_USER")
+EMAIL_HOST_USER = config("EMAIL_HOST_USER", default="default@email.com")
 EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
