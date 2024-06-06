22cb3588be6d6930c1d35821653171c4f6e31804
Mateusz Świetlik <matiks@gmail.com>
2024-06-06 22:14:53 +0200

    improved test_is_online

--- users/tests/test_online_status.py
+++ users/tests/test_online_status.py
@@ -1,13 +1,14 @@
 from datetime import timedelta
 
 import pytest
+from django.urls import reverse
 from django.utils import timezone
 
 from users.models import User
 
 
 @pytest.mark.django_db()
-def test_is_online(user: User, settings: list) -> None:
+def test_is_online(user: User, settings: list, client) -> None:
     settings.LAST_ACTIVITY_ONLINE_LIMIT_MINUTES = 15
     # Initially, the user's last_activity should be datetime.now
     assert user.is_online
@@ -19,6 +20,12 @@ def test_is_online(user: User, settings: list) -> None:
     user.last_activity = timezone.now() - timedelta(minutes=16)
     user.save()
     assert not user.is_online
+
+    client.force_login(user)
+    client.get(reverse("home"))
+    user.refresh_from_db()
+
+    assert user.is_online
 
 
 @pytest.mark.django_db()
@@ -46,3 +53,4 @@ def test_last_activity_ago(user: User) -> None:
     user.last_activity = timezone.now() - timedelta(days=3)
     user.save()
     assert user.last_activity_ago == "3 days ago"
+
