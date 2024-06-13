673a6f7691bf0982b9c82d2dec4ac3f6bc6b20d3
Jan Gregorczyk <jnk.gregorczyk@gmail.com>
2024-06-13 13:11:53 +0200

    Add username field to User model

--- users/models.py
+++ users/models.py
@@ -11,22 +11,22 @@ from django.utils import timezone
 class UserManager(BaseUserManager):
     use_in_migrations: bool = True
 
-    def _create_user(self: "UserManager", email: str, password: str, **extra_fields: int) -> "User":
+    def _create_user(self: "UserManager", username:str, email: str, password: str, **extra_fields: int) -> "User":
         if not email:
             message: str = "Users must have an email address"
             raise ValueError(message)
         email = self.normalize_email(email)
-        user: User = self.model(email=email, **extra_fields)
+        user: User = self.model(username=username, email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_user(self: "UserManager", email: str, password: str, **extra_fields: int) -> "User":
+    def create_user(self: "UserManager", username:str, email: str, password: str, **extra_fields: int) -> "User":
         extra_fields.setdefault("is_staff", False)
         extra_fields.setdefault("is_superuser", False)
-        return self._create_user(email, password, **extra_fields)
+        return self._create_user(username, email, password, **extra_fields)
 
-    def create_superuser(self: "UserManager", email: str, password: str, **extra_fields: int) -> "User":
+    def create_superuser(self: "UserManager", username:str, email: str, password: str, **extra_fields: int) -> "User":
 
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
@@ -38,14 +38,12 @@ class UserManager(BaseUserManager):
             message: str = "Superuser must have is_superuser=True."
             raise ValueError(message)
 
-        return self._create_user(email, password, **extra_fields)
+        return self._create_user(username, email, password, **extra_fields)
 
 
 class User(AbstractUser):
-    USERNAME_FIELD = "email"
-    REQUIRED_FIELDS: ClassVar[list[int]] = []
+    REQUIRED_FIELDS: ClassVar[list[str]] = ["email"]
     objects = UserManager()
-    username: None = None
     email: str = models.EmailField(unique=True)
     last_activity = models.DateTimeField(auto_now_add=True)
 
--- users/tests/test_user_model.py
+++ users/tests/test_user_model.py
@@ -11,6 +11,7 @@ HTTP_SUCCESS = 200
 
 @pytest.mark.django_db()
 def test_create_user(user: User) -> None:
+    assert user.username == "test_user"
     assert user.email == "test@example.com"
     assert user.is_staff is False
     assert user.is_superuser is False
@@ -21,6 +22,7 @@ def test_create_user(user: User) -> None:
 
 @pytest.mark.django_db()
 def test_create_superuser(admin_user: User) -> None:
+    assert admin_user.username == "admin"
     assert admin_user.email == "admin@example.com"
     assert admin_user.is_staff is True
     assert admin_user.is_superuser is True
@@ -32,25 +34,25 @@ def test_create_superuser(admin_user: User) -> None:
 @pytest.mark.django_db()
 def test_create_user_missing_email(generated_password: str) -> None:
     with pytest.raises(ValueError, match="Users must have an email address"):
-        User.objects.create_user(email="", password=generated_password)
+        User.objects.create_user(username="username", email="", password=generated_password)
 
 
 @pytest.mark.django_db()
 def test_create_superuser_missing_email(generated_password: str) -> None:
     with pytest.raises(ValueError, match="Users must have an email address"):
-        User.objects.create_superuser(email="", password=generated_password)
+        User.objects.create_superuser(username="admin", email="", password=generated_password)
 
 
 @pytest.mark.django_db()
 def test_create_superuser_not_staff(generated_password: str) -> None:
     with pytest.raises(ValueError, match="Superuser must have is_staff=True."):
-        User.objects.create_superuser(email="admin@example.com", password=generated_password, is_staff=False)
+        User.objects.create_superuser(username="admin", email="admin@example.com", password=generated_password, is_staff=False)
 
 
 @pytest.mark.django_db()
 def test_create_superuser_not_superuser(generated_password: str) -> None:
     with pytest.raises(ValueError, match="Superuser must have is_superuser=True."):
-        User.objects.create_superuser(email="admin@example.com", password=generated_password, is_superuser=False)
+        User.objects.create_superuser(username="admin", email="admin@example.com", password=generated_password, is_superuser=False)
 
 
 def test_login_user_view_get(client: Client) -> None:
