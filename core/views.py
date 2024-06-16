5dd61b1efb854f458a7de7fcad6941a643b80085
Mateusz Świetlik <matiks@gmail.com>
2024-06-16 15:29:02 +0200

    created PostVoteView and modified template to allow user to vote for specific post

--- core/views.py
+++ core/views.py
@@ -1,7 +1,11 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models import QuerySet
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import get_object_or_404, redirect
+from django.views import View
 from django.views.generic import DetailView, ListView
 
-from .models import Post
+from .models import Post, PostVote
 
 
 class PostListlView(ListView):
@@ -31,3 +35,13 @@ class PostDetailView(DetailView):
         obj = super().get_object(queryset=queryset)
         obj.update_display_counter()
         return obj
+
+
+class PostVoteView(LoginRequiredMixin, View):
+    def post(self: "PostVoteView", request: HttpRequest, pk: int, vote_type: str) -> HttpResponse:
+        post = get_object_or_404(Post, pk=pk)
+        if vote_type == "up":
+            post.vote(request.user, PostVote.UPVOTE)
+        elif vote_type == "down":
+            post.vote(request.user, PostVote.DOWNVOTE)
+        return redirect("post-detail", pk=post.id)
