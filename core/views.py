d5cd7f35d8c4e5828eb2a66839af6d49b8b17586
tgrycuk <40910668+tgrycuk@users.noreply.github.com>
2024-06-18 11:57:51 +0200

    Implemented PostListView and organized some static files (not final) (#47)
    * Implemented PostListView and organized some static files (not final)
    
    * update stripe to extra_js
    
    ---------
    
    Co-authored-by: Tomasz Grycuk <t.grycuk@gmail.com>

--- core/views.py
+++ core/views.py
@@ -10,19 +10,14 @@ from .models import Post, PostVote
 
 class PostListlView(ListView):
 
-    """Provide a temporary view for internal testing of the post view count feature.
-
-    This view is not intended for production use and should be accessed only by developers.
-    """
-
     model = Post
-    template_name = "post-list.html"
+    template_name = "core/post-list.html"
     context_object_name = "posts"
 
 
 class PostDetailView(DetailView):
     model = Post
-    template_name = "post-detail.html"
+    template_name = "core/post-detail.html"
     context_object_name = "post"
 
     def get_object(self: "Post", queryset: QuerySet[Post] | None = None) -> Post:
