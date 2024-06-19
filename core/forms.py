e7e3261798cc58908c87b18fd2d0a7b1aaa87a5f
Mateusz Świetlik <matiks@gmail.com>
2024-06-19 19:07:48 +0200

    udpated CommentForm and view

--- core/forms.py
+++ core/forms.py
@@ -10,7 +10,7 @@ class CommentForm(forms.ModelForm):
 
     class Meta:
         model: ClassVar = Post
-        fields: ClassVar = ["content"]
+        fields: ClassVar = ["content", "parent_id"]
         widgets: ClassVar = {
             "content": forms.Textarea(attrs={"rows": 3, "class": "form-control bg-dark text-light"}),
         }
