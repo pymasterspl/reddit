4d7f3fcdcf862bef4186b1b8ff1152e71ca24562
Mateusz Świetlik <matiks@gmail.com>
2024-06-20 22:02:22 +0200

    quick fix

--- core/forms.py
+++ core/forms.py
@@ -1,20 +1,11 @@
-from typing import ClassVar
-
 from django import forms
 
 
 class CommentForm(forms.Form):
-    content = forms.CharField(required=True, widget=forms.Textarea(
+    content = forms.CharField(label="Add a comment", required=True, widget=forms.Textarea(
         attrs={
             "rows": 3,
             "class": "form-control bg-dark text-light",
         },
-    ),
-                              )
+    ))
     parent_id = forms.IntegerField(required=True, widget=forms.HiddenInput)
-
-    class Meta:
-        fields: ClassVar[list[str]] = ["content", "parent_id"]
-        labels: ClassVar[dict[str, str]] = {
-            "content": "Add a comment:",
-        }
