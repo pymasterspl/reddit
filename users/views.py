0ee20843c9e62a26768b123209797a768f1755df
Pawelooo <paweloo0147@gmail.com>
2024-06-20 17:03:15 +0200

    Send mail and view to acivate

--- users/views.py
+++ users/views.py
@@ -1,13 +1,24 @@
 from typing import Any
 
+from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.views import LoginView, LogoutView, TemplateView
+from django.contrib.sites.shortcuts import get_current_site
+from django.core.mail import send_mail
 from django.forms import Form
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpRequest, HttpResponse, request
+from django.shortcuts import redirect, render
+from django.template.loader import render_to_string
 from django.urls import reverse_lazy
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.views import View
 from django.views.generic import FormView
+from django.utils.encoding import force_str
 
+from .tokens import account_activation_token
+from core.models import User
 from .forms import UserRegistrationForm
 
 
@@ -37,9 +48,27 @@ class UserRegistrationView(FormView):
     success_url = reverse_lazy("home-page")
 
     def form_valid(self: "UserRegistrationView", form: UserRegistrationForm) -> HttpResponse:
-        form.save()
-        email = form.cleaned_data.get("email")
-        messages.success(self.request, f"Account created for {email}!")
+        user = form.save(commit=False)
+        user.is_active = False
+        user.save()
+        uid = urlsafe_base64_encode(force_bytes(user.pk))
+        token = account_activation_token.make_token(user)
+        activation_link = f"http://{self.request.get_host()}/{uid}/{token}/"
+        current_site = get_current_site(self.request)
+        send_mail(
+            'Confirm your registration',
+            f'Please click on the following link to confirm your registration {activation_link}',
+            settings.EMAIL_HOST_USER,
+            [user.email],
+            fail_silently=False,
+            html_message=render_to_string('users/account_activation_email.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid': uid,
+                'token': token,
+            })
+        )
+        messages.success(self.request, f"Account created for {user.email}! Please confirm your email to activate your account.")
         return super().form_valid(form)
 
 
@@ -49,3 +78,23 @@ class CustomLogoutView(LogoutView):
     def dispatch(self: "LogoutView", request: HttpRequest, *args: tuple, **kwargs: dict[str, Any]) -> HttpResponse:
         messages.add_message(request, messages.SUCCESS, "You have successfully logged out.")
         return super().dispatch(request, *args, **kwargs)
+
+
+class ActivateUser(View):
+
+    def get(self, request, uidb64, token, *args, **kwargs):
+        try:
+            uid = force_str(urlsafe_base64_decode(uidb64))
+            user = User.objects.get(pk=uid)
+            if account_activation_token.check_token(user, token):
+                user.is_active = True
+                user.save()
+                messages.success(request,
+                                 "Your account has been activated, you can now login!")
+
+                return redirect('login')
+            else:
+                return render(request, 'users/account_activation_invalid.html')
+        except User.DoesNotExist:
+            messages.error(request, "Invalid activation link or account already activated!")
+            return redirect('home-page')
