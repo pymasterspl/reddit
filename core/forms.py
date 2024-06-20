b0915daa61e659e8b9669bf9295df7c3c1a75001
Mateusz Świetlik <matiks@gmail.com>
2024-06-20 21:53:40 +0200

    ruff fixes

--- core/forms.py
+++ core/forms.py
@@ -1,12 +1,20 @@
+from typing import ClassVar
+
 from django import forms
 
 
 class CommentForm(forms.Form):
-    content = forms.CharField(required=True, widget=forms.Textarea(attrs={"rows": 3, "class": "form-control bg-dark text-light"}))
+    content = forms.CharField(required=True, widget=forms.Textarea(
+        attrs={
+            "rows": 3,
+            "class": "form-control bg-dark text-light",
+        },
+    ),
+                              )
     parent_id = forms.IntegerField(required=True, widget=forms.HiddenInput)
 
     class Meta:
-        fields = ["content", "parent_id"]
-        labels = {
+        fields: ClassVar[list[str]] = ["content", "parent_id"]
+        labels: ClassVar[dict[str, str]] = {
             "content": "Add a comment:",
         }
