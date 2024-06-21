6dafb4be652a087138ea19570075c3424c713ed3
memorisanka <j.krawczyk87@gmail.com>
2024-06-21 22:13:45 +0200

    feat: add form for community creating

--- core/forms.py
+++ core/forms.py
@@ -1,4 +1,9 @@
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
 from django import forms
+from django.utils.text import slugify
+
+from core.models import Community
 
 
 class CommentForm(forms.Form):
@@ -9,3 +14,22 @@ class CommentForm(forms.Form):
         },
     ))
     parent_id = forms.IntegerField(required=True, widget=forms.HiddenInput)
+
+
+class CommunityForm(forms.ModelForm):
+    class Meta:
+        model = Community
+        fields = ['name']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'post'
+        self.helper.add_input(Submit('submit', 'Create Community'))
+
+    def save(self, commit=True):
+        instance = super().save(commit=False)
+        instance.slug = slugify(instance.name)
+        if commit:
+            instance.save()
+        return instance
