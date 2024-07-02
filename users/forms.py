84a838b03d4b0a4326fd2d33d2b421abb336aaad
Tomasz Grycuk <t.grycuk@gmail.com>
2024-07-02 17:47:59 +0200

    settings.MAX_AVATAR_SIZE_MB

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
 from typing import ClassVar
 
 from django import forms
+from django.conf import settings
 from django.contrib.auth.forms import UserCreationForm
 from django.core.exceptions import ValidationError
 
@@ -8,7 +9,7 @@ from .models import User
 
 
 def validate_avatar(file: any) -> None:
-    max_size_mb = 2
+    max_size_mb = settings.MAX_AVATAR_SIZE_MB
     if file.size > max_size_mb * 1024 * 1024:
         message = f"Avatar file size must be under {max_size_mb}MB"
         raise ValidationError(message)
