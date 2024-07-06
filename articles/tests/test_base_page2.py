ebbfc665d5c73a339d7ea43889b15d8cd8f396e2
Jacek Ostański <jacek@ivolution.pl>
2024-07-06 16:01:46 +0200

    test

---
+++ articles/tests/test_base_page2.py
@@ -0,0 +1,13 @@
+import pytest
+from django.test import Client
+from django.urls import reverse
+
+
+@pytest.mark.django_db()
+def test_privacy_police_view() -> None:
+    client = Client()
+    url = reverse("home-page")
+    http_ok = 200
+    response = client.get(url)
+    assert response.status_code == http_ok
+    assert "base.html" in [t.name for t in response.templates]
