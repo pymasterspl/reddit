1cf0fceee82d5170e7717f99b42a10f1f76e51fa
Mateusz Świetlik <matiks@gmail.com>
2024-07-07 18:43:16 +0200

    added tests for adding nested comments

--- core/tests/test_views.py
+++ core/tests/test_views.py
@@ -103,3 +103,44 @@ def test_add_comment_unauthorized(client: Client, post: Post) -> None:
     response = client.post(reverse("post-detail", kwargs={"pk": post.pk}), data=data)
     assert response.status_code == 302
     assert reverse("login") in response.url
+
+
+def test_add_nested_comment_valid(client: Client, another_user: User, post: Post, comment: Post) -> None:
+    data = {
+        "parent_id": comment.pk,
+        "content": "This is a test nested comment content.",
+    }
+    client.force_login(another_user)
+    assert post.children_count == 1
+    assert comment.children_count == 0
+    response = client.post(reverse("post-detail", kwargs={"pk": post.pk}), data=data, follow=True)
+    assert response.status_code == 200
+    assert post.children_count == 2
+    assert comment.children_count == 1
+
+    new_comment = Post.objects.get(content=data['content'])
+    assert new_comment.author == another_user
+    assert new_comment.parent == comment
+
+
+def test_add_nested_comment_invalid(client: Client, user: User, post: Post, comment: Post) -> None:
+    data = {
+        "parent_id": comment.pk,
+        "content": ""
+    }
+    client.force_login(user)
+    response = client.post(reverse("post-detail", kwargs={"pk": post.pk}), data=data)
+    assert response.status_code == 200
+    form = response.context["form"]
+    assert len(form.errors) == 1
+    assert "This field is required." in form.errors["content"]
+
+
+def test_add_nested_comment_unauthorized(client: Client, post: Post, comment: Post) -> None:
+    data = {
+        "parent_id": comment.pk,
+        "content": "This is a test nested comment content.",
+    }
+    response = client.post(reverse("post-detail", kwargs={"pk": post.pk}), data=data)
+    assert response.status_code == 302
+    assert reverse("login") in response.url
