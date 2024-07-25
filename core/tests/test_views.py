3c06cc805a9a29e67e9116ca3d0ad3c36a217013
Pawelooo <paweloo0147@gmail.com>
2024-07-25 15:19:43 +0200

    Correct tests and and new tests

--- core/tests/test_views.py
+++ core/tests/test_views.py
@@ -1,15 +1,14 @@
 import secrets
 import string
-import tempfile
 
 import pytest
+from django.conf import Settings
 from django.contrib.auth import get_user_model
-from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import Client
 from django.urls import reverse
-from PIL import Image
 
 from core.models import Community, Post
+from utils.create_test_avatar import create_test_avatar
 
 pytestmark = pytest.mark.django_db
 
@@ -35,51 +34,55 @@ def user(client: Client) -> User:
 
 
 @pytest.fixture()
+def user_with_avatar(client: Client) -> User:
+    password = generate_random_password()
+    user = User.objects.create_user(
+        email="test_user@example.com",
+        nickname="TestUser",
+        password=password,
+    )
+    create_test_avatar(user)
+    client.login(email=user.email, password=user.password)
+
+    return user
+
+
+@pytest.fixture()
 def community() -> Community:
     return Community.objects.create(name="Test Community", is_active=True)
 
 
+@pytest.fixture()
+def default_avatar_url(settings: Settings) -> None:
+    return settings.DEFAULT_AVATAR_URL
+
+
 def test_add_post_valid(client: Client, user: User, community: Community) -> None:
-    with tempfile.NamedTemporaryFile(suffix=".jpg", delete=False) as tmp_file:
-        image = Image.new("RGB", (100, 100), color=(73, 109, 137))
-        image.save(tmp_file, format="JPEG")
-        tmp_file.seek(0)
+    create_test_avatar(user)
 
-        avatar_file = SimpleUploadedFile(name="test_avatar.jpg", content=tmp_file.read(), content_type="image/jpeg")
-
-        user.avatar = avatar_file
-        user.save()
-
-        data = {
-            "community": community.pk,
-            "title": "Test Post Title",
-            "content": "This is a test post content.",
-        }
-        client.force_login(user)
-        response = client.post(reverse("post-create"), data=data, follow=True)
-        assert response.status_code == 200
-        assert response.context["post"].author == user
-        assert response.context["post"].title == data["title"]
-        assert response.context["post"].content == data["content"]
+    data = {
+        "community": community.pk,
+        "title": "Test Post Title",
+        "content": "This is a test post content.",
+    }
+    client.force_login(user)
+    response = client.post(reverse("post-create"), data=data, follow=True)
+    assert response.status_code == 200
+    assert response.context["post"].author == user
+    assert response.context["post"].title == data["title"]
+    assert response.context["post"].content == data["content"]
+    assert response.context["post"].author.avatar == user.avatar
 
 
 def test_add_post_invalid(client: Client, user: User, community: Community) -> None:
-    with tempfile.NamedTemporaryFile(suffix=".jpg", delete=False) as tmp_file:
-        image = Image.new("RGB", (100, 100), color=(73, 109, 137))
-        image.save(tmp_file, format="JPEG")
-        tmp_file.seek(0)
-
-        avatar_file = SimpleUploadedFile(name="test_avatar.jpg", content=tmp_file.read(), content_type="image/jpeg")
-
-        user.avatar = avatar_file
-        user.save()
-        data = {"title": ""}
-        client.force_login(user)
-        response = client.post(reverse("post-create"), data=data)
-        assert response.status_code == 200
-        form = response.context["form"]
-        assert "This field is required." in form.errors["title"]
-        assert "This field is required." in form.errors["content"]
+    create_test_avatar(user)
+    data = {"title": ""}
+    client.force_login(user)
+    response = client.post(reverse("post-create"), data=data)
+    assert response.status_code == 200
+    form = response.context["form"]
+    assert "This field is required." in form.errors["title"]
+    assert "This field is required." in form.errors["content"]
 
 
 def test_add_post_unauthorized(client: Client, community: Community) -> None:
@@ -94,66 +97,44 @@ def test_add_post_unauthorized(client: Client, community: Community) -> None:
 
 
 def test_add_comment_valid(client: Client, user: User, post: Post) -> None:
-    with tempfile.NamedTemporaryFile(suffix=".jpg", delete=False) as tmp_file:
-        image = Image.new("RGB", (100, 100), color=(73, 109, 137))
-        image.save(tmp_file, format="JPEG")
-        tmp_file.seek(0)
+    create_test_avatar(user)
 
-        avatar_file = SimpleUploadedFile(name="test_avatar.jpg", content=tmp_file.read(), content_type="image/jpeg")
-
-        user.avatar = avatar_file
-        user.save()
-        data = {
-            "parent_id": post.pk,
-            "content": "This is a test comment content.",
-        }
-        client.force_login(user)
-        assert post.children_count == 0
-        assert post.get_comments().count() == 0
-        response = client.post(reverse("post-detail", kwargs={"pk": post.pk}), data=data, follow=True)
-        assert response.status_code == 200
-        assert post.children_count == 1
-        assert post.get_comments().count() == 1
-        post.refresh_from_db()
-        assert response.context["comments"][0].author == user
-        assert response.context["comments"][0].content == data["content"]
+    data = {
+        "parent_id": post.pk,
+        "content": "This is a test comment content.",
+    }
+    client.force_login(user)
+    assert post.children_count == 0
+    assert post.get_comments().count() == 0
+    response = client.post(reverse("post-detail", kwargs={"pk": post.pk}), data=data, follow=True)
+    assert response.status_code == 200
+    assert post.children_count == 1
+    assert post.get_comments().count() == 1
+    post.refresh_from_db()
+    assert response.context["comments"][0].author == user
+    assert response.context["comments"][0].content == data["content"]
 
 
 def test_add_comment_invalid(client: Client, user: User, post: Post) -> None:
-    with tempfile.NamedTemporaryFile(suffix=".jpg", delete=False) as tmp_file:
-        image = Image.new("RGB", (100, 100), color=(73, 109, 137))
-        image.save(tmp_file, format="JPEG")
-        tmp_file.seek(0)
-
-        avatar_file = SimpleUploadedFile(name="test_avatar.jpg", content=tmp_file.read(), content_type="image/jpeg")
-
-        user.avatar = avatar_file
-        user.save()
-        data = {"parent_id": post.pk, "content": ""}
-        client.force_login(user)
-        response = client.post(reverse("post-detail", kwargs={"pk": post.pk}), data=data)
-        assert response.status_code == 200
-        form = response.context["form"]
-        assert len(form.errors) == 1
-        assert "This field is required." in form.errors["content"]
+    create_test_avatar(user)
+    data = {"parent_id": post.pk, "content": ""}
+    client.force_login(user)
+    response = client.post(reverse("post-detail", kwargs={"pk": post.pk}), data=data)
+    assert response.status_code == 200
+    form = response.context["form"]
+    assert len(form.errors) == 1
+    assert "This field is required." in form.errors["content"]
 
 
 def test_add_comment_valid_special_characters(client: Client, user: User, post: Post) -> None:
-    with tempfile.NamedTemporaryFile(suffix=".jpg", delete=False) as tmp_file:
-        image = Image.new("RGB", (100, 100), color=(73, 109, 137))
-        image.save(tmp_file, format="JPEG")
-        tmp_file.seek(0)
+    create_test_avatar(user)
 
-        avatar_file = SimpleUploadedFile(name="test_avatar.jpg", content=tmp_file.read(), content_type="image/jpeg")
-
-        user.avatar = avatar_file
-        user.save()
-        data = {"parent_id": post.pk, "content": "This is a test comment with special characters! 😊🚀✨"}
-        client.force_login(user)
-        response = client.post(reverse("post-detail", kwargs={"pk": post.pk}), data=data, follow=True)
-        assert response.status_code == 200
-        new_comment = Post.objects.get(content=data["content"])
-        assert new_comment.content == data["content"]
+    data = {"parent_id": post.pk, "content": "This is a test comment with special characters! 😊🚀✨"}
+    client.force_login(user)
+    response = client.post(reverse("post-detail", kwargs={"pk": post.pk}), data=data, follow=True)
+    assert response.status_code == 200
+    new_comment = Post.objects.get(content=data["content"])
+    assert new_comment.content == data["content"]
 
 
 def test_add_comment_unauthorized(client: Client, post: Post) -> None:
@@ -167,55 +148,40 @@ def test_add_comment_unauthorized(client: Client, post: Post) -> None:
 
 
 def test_add_nested_comment_valid(client: Client, user: User, post: Post, comment: Post) -> None:
-    with tempfile.NamedTemporaryFile(suffix=".jpg", delete=False) as tmp_file:
-        image = Image.new("RGB", (100, 100), color=(73, 109, 137))
-        image.save(tmp_file, format="JPEG")
-        tmp_file.seek(0)
+    create_test_avatar(user)
 
-        avatar_file = SimpleUploadedFile(name="test_avatar.jpg", content=tmp_file.read(), content_type="image/jpeg")
+    data = {
+        "parent_id": comment.pk,
+        "content": "This is a test nested comment content.",
+    }
+    client.force_login(user)
+    assert post.children_count == 1
+    assert post.get_comments().count() == 1
+    assert comment.children_count == 0
+    assert comment.get_comments().count() == 0
+    response = client.post(reverse("post-detail", kwargs={"pk": post.pk}), data=data, follow=True)
+    assert response.status_code == 200
+    post.refresh_from_db()
+    comment.refresh_from_db()
+    assert post.children_count == 2
+    assert post.get_comments().count() == 1
+    assert comment.children_count == 1
+    assert comment.get_comments().count() == 1
 
-        user.avatar = avatar_file
-        user.save()
-        data = {
-            "parent_id": comment.pk,
-            "content": "This is a test nested comment content.",
-        }
-        client.force_login(user)
-        assert post.children_count == 1
-        assert post.get_comments().count() == 1
-        assert comment.children_count == 0
-        assert comment.get_comments().count() == 0
-        response = client.post(reverse("post-detail", kwargs={"pk": post.pk}), data=data, follow=True)
-        assert response.status_code == 200
-        post.refresh_from_db()
-        comment.refresh_from_db()
-        assert post.children_count == 2
-        assert post.get_comments().count() == 1
-        assert comment.children_count == 1
-        assert comment.get_comments().count() == 1
-
-        new_comment = Post.objects.get(content=data["content"])
-        assert new_comment.author == user
-        assert new_comment.parent == comment
+    new_comment = Post.objects.get(content=data["content"])
+    assert new_comment.author == user
+    assert new_comment.parent == comment
 
 
 def test_add_nested_comment_invalid(client: Client, user: User, post: Post, comment: Post) -> None:
-    with tempfile.NamedTemporaryFile(suffix=".jpg", delete=False) as tmp_file:
-        image = Image.new("RGB", (100, 100), color=(73, 109, 137))
-        image.save(tmp_file, format="JPEG")
-        tmp_file.seek(0)
-
-        avatar_file = SimpleUploadedFile(name="test_avatar.jpg", content=tmp_file.read(), content_type="image/jpeg")
-
-        user.avatar = avatar_file
-        user.save()
-        data = {"parent_id": comment.pk, "content": ""}
-        client.force_login(user)
-        response = client.post(reverse("post-detail", kwargs={"pk": post.pk}), data=data)
-        assert response.status_code == 200
-        form = response.context["form"]
-        assert len(form.errors) == 1
-        assert "This field is required." in form.errors["content"]
+    create_test_avatar(user)
+    data = {"parent_id": comment.pk, "content": ""}
+    client.force_login(user)
+    response = client.post(reverse("post-detail", kwargs={"pk": post.pk}), data=data)
+    assert response.status_code == 200
+    form = response.context["form"]
+    assert len(form.errors) == 1
+    assert "This field is required." in form.errors["content"]
 
 
 def test_add_nested_comment_unauthorized(client: Client, post: Post, comment: Post) -> None:
@@ -229,18 +195,7 @@ def test_add_nested_comment_unauthorized(client: Client, post: Post, comment: Po
 
 
 def test_add_deeply_nested_comment_valid(client: Client, user: User, post: Post) -> None:
-    with tempfile.NamedTemporaryFile(suffix=".jpg", delete=False) as tmp_file:
-        image = Image.new("RGB", (100, 100), color=(73, 109, 137))
-        image.save(tmp_file, format="JPEG")
-        tmp_file.seek(0)
-
-        avatar_file = SimpleUploadedFile(name="test_avatar.jpg", content=tmp_file.read(), content_type="image/jpeg")
-
-        user.avatar = avatar_file
-        user.save()
-
-    tmp_file.close()
-
+    create_test_avatar(user)
     client.force_login(user)
 
     parent_comment = post
@@ -257,3 +212,30 @@ def test_add_deeply_nested_comment_valid(client: Client, user: User, post: Post)
     assert post.children_count == 10
     assert parent_comment.children_count == 0
     assert parent_comment.parent.children_count == 1
+
+
+@pytest.mark.django_db()
+def test_post_user_avatar_display(client: Client, community: Community, user_with_avatar: User) -> None:
+    data = {
+        "community": community.pk,
+        "title": "Test Post Title",
+        "content": "This is a test post content.",
+    }
+    client.force_login(user_with_avatar)
+    response = client.post(reverse("post-create"), data=data, follow=True)
+    post = response.context["post"]
+    expected_avatar_url = user_with_avatar.avatar.url
+    assert post.author.avatar.url == expected_avatar_url
+
+
+@pytest.mark.django_db()
+def test_post_user_without_avatar(client: Client, community: Community, user: User, default_avatar_url: str) -> None:
+    data = {
+        "community": community.pk,
+        "title": "Test Post Title",
+        "content": "This is a test post content.",
+    }
+    client.force_login(user)
+    response = client.post(reverse("post-create"), data=data, follow=True)
+    post = response.context["post"]
+    assert post.author.avatar_url == default_avatar_url
