dd8869e3061401fcfdc4679a5b8620850efd286d
Jan Gregorczyk <jnk.gregorczyk@gmail.com>
2024-08-09 21:10:47 +0200

    fix: migration dependency

--- users/migrations/0009_user_avatar.py
+++ users/migrations/0009_user_avatar.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('users', '0008_alter_user_nickname'),
+        ('users', '0008_profile_usersettings_alter_user_nickname_and_more'),
     ]
 
     operations = [
