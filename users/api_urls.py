897e9a91a0c162a85bf8515dabf1003151c410a8
Jan Gregorczyk <jnk.gregorczyk@gmail.com>
2024-09-23 14:59:48 +0200

    chore: add comment with urlpatterns for dj-rest-auth endpoints

--- users/api_urls.py
+++ users/api_urls.py
@@ -4,3 +4,17 @@ from django.urls import include, path
 urlpatterns = [
     path("", include("dj_rest_auth.urls"))
 ]
+
+"""
+dj_rest_auth.urls below:
+
+# URLs that do not require a session or valid token
+path('password/reset/', PasswordResetView.as_view(), name='rest_password_reset'),
+path('password/reset/confirm/', PasswordResetConfirmView.as_view(), name='rest_password_reset_confirm'),
+path('login/', LoginView.as_view(), name='rest_login'),
+
+# URLs that require a user to be logged in with a valid session / token.
+path('logout/', LogoutView.as_view(), name='rest_logout'),
+path('user/', UserDetailsView.as_view(), name='rest_user_details'),
+path('password/change/', PasswordChangeView.as_view(), name='rest_password_change'),
+"""
