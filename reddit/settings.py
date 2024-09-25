7e65bd69ff2f9644cadde3897c489536461b1c45
Jan Gregorczyk <jnk.gregorczyk@gmail.com>
2024-09-25 16:51:03 +0200

    chore: set-up token authentication class

--- reddit/settings.py
+++ reddit/settings.py
@@ -171,7 +171,13 @@ LIMIT_WARNINGS = 5
 LOGIN_URL = reverse_lazy("login")
 
 
-REST_FRAMEWORK = {"DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination", "PAGE_SIZE": 10}
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
 DEFAULT_AVATAR_URL = "/media/users_avatars/default.png"
 
 
