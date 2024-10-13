7114b87199ba05347496c6dadef929627b0278c4
konrad-paszek <konradpaszek.dev@gmail.com>
2024-10-13 17:24:11 +0200

    test

--- users/admin.py
+++ users/admin.py
@@ -42,7 +42,7 @@ class CustomUserAdmin(DjangoUserAdmin):
             },
         ),
     )
-
+#test
     def is_online(self: "CustomUserAdmin", obj: User) -> bool | str:
         return obj.is_online
 
