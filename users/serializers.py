0d15c0b53d81c352070c8d967358dd733873eb1a
konrad-paszek <konradpaszek.dev@gmail.com>
2025-01-11 19:01:28 +0100

    review fix

--- users/serializers.py
+++ users/serializers.py
@@ -6,22 +6,18 @@ from .models import Profile, User, UserSettings
 from .utils import user_creation
 
 
-class BaseUserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields: typing.ClassVar[list] = ["nickname", "email"]
-
-
-class UserSerializer(BaseUserSerializer):
+class UserSerializer(serializers.ModelSerializer):
     user_bio = serializers.CharField(source="profile.bio", read_only=True)
     avatar = serializers.CharField(source="profile.avatar_url", read_only=True)
     banner = serializers.CharField(source="profile.banner", read_only=True)
     post_karma = serializers.IntegerField(source="profile.post_karma", read_only=True)
     comment_karma = serializers.IntegerField(source="profile.comment_karma", read_only=True)
 
-    class Meta(BaseUserSerializer.Meta):
+    class Meta:
+        model = User
         fields: typing.ClassVar[list] = [
-            *BaseUserSerializer.Meta.fields,
+            "nickname",
+            "email",
             "avatar",
             "user_bio",
             "banner",
@@ -42,13 +38,13 @@ class UserSettingsSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 
-class UserRegistrationSerializer(BaseUserSerializer):
+class UserRegistrationSerializer(serializers.ModelSerializer):
     message = serializers.SerializerMethodField(read_only=True)
     password = serializers.CharField(write_only=True, required=True, style={"input_type": "password"})
     password2 = serializers.CharField(write_only=True, required=True, style={"input_type": "password"})
 
-    class Meta(BaseUserSerializer.Meta):
-        fields: typing.ClassVar[list] = [*BaseUserSerializer.Meta.fields, "password", "password2", "message"]
+    class Meta:
+        fields: typing.ClassVar[list] = ["nickname", "email", "password", "password2", "message"]
 
     def validate(self: "UserRegistrationSerializer", attrs: dict) -> dict:
         if attrs["password"] != attrs["password2"]:
