832d189452030bdeb5945d13304911146caac90c
konrad-paszek <konradpaszek.dev@gmail.com>
2025-02-02 16:01:52 +0100

    review fix

--- core/profile_urls.py
+++ core/profile_urls.py
@@ -4,6 +4,6 @@ from core.views import SavedPostListView, UserCommentsListView, UserPostListView
 
 profile_urlpatterns = [
     path("profile/saved-post-list/", SavedPostListView.as_view(), name="saved_posts"),
-    path("profile/user-comments-list", UserCommentsListView.as_view(), name="user_comments"),
+    path("profile/user-comments-list/", UserCommentsListView.as_view(), name="user_comments"),
     path("profile/posts/", UserPostListView.as_view(), name="user_posts"),
 ]
