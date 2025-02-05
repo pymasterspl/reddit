170636aeea2773689b6b735f45671a5ffe95f776
revoxer <revoxers@gmail.com>
2025-02-05 11:59:22 +0100

    Tests upgrades and add two new tests

--- users/tests/test_vote_content.py
+++ users/tests/test_vote_content.py
@@ -3,76 +3,77 @@ from django.contrib.auth import get_user_model
 from django.test import Client
 from django.urls import reverse
 
-from core.models import Post, PostVote
+from core.models import PostVote
 
 User = get_user_model()
 
 
 @pytest.mark.django_db()
-def test_upvoted_post(client: Client, user: User, post: Post, upvote_post: PostVote) -> None:
+def test_upvoted_post(client: Client, user: User, upvote_post: PostVote) -> None:
     url = reverse("user_upvoted")
     client.force_login(user)
     response = client.get(url)
-    post.refresh_from_db()
-    upvote_post.refresh_from_db()
     assert response.status_code == 200
-    assert upvote_post.post == post
-    assert upvote_post.choice == PostVote.UPVOTE
-    assert post.up_votes == 1
+    assert upvote_post.post in response.context["posts"]
+    assert len(response.context["posts"]) == 1
 
 
 @pytest.mark.django_db()
-def test_upvoted_comment(client: Client, user: User, comment: Post, upvote_comment: PostVote) -> None:
+def test_upvoted_comment(client: Client, user: User, upvote_comment: PostVote) -> None:
     url = reverse("user_upvoted")
     client.force_login(user)
     response = client.get(url)
-    comment.refresh_from_db()
-    upvote_comment.refresh_from_db()
     assert response.status_code == 200
-    assert upvote_comment.post == comment
-    assert upvote_comment.choice == PostVote.UPVOTE
-    assert comment.up_votes == 1
+    assert upvote_comment.post in response.context["posts"]
+    assert len(response.context["posts"]) == 1
 
 
 @pytest.mark.django_db()
-def test_filter_post(client: Client, user: User, post: Post, upvote_post: PostVote) -> None:
+def test_upvoted_filter_post(client: Client, user: User, upvote_post: PostVote) -> None:
     url = reverse("user_upvoted") + "?filter=post"
     client.force_login(user)
     response = client.get(url)
-    post.refresh_from_db()
-    upvote_post.refresh_from_db()
     assert response.status_code == 200
-    assert post in response.context["posts"]
+    assert upvote_post.post in response.context["posts"]
+    assert len(response.context["posts"]) == 1
 
 
 @pytest.mark.django_db()
-def test_filter_comment(client: Client, user: User, comment: Post, upvote_comment: PostVote) -> None:
+def test_upvoted_filter_post_empty(client: Client, user: User, upvote_comment: PostVote) -> None:
+    url = reverse("user_upvoted") + "?filter=post"
+    client.force_login(user)
+    response = client.get(url)
+    assert response.status_code == 200
+    assert upvote_comment.post not in response.context["posts"]
+    assert len(response.context["posts"]) == 0
+
+
+@pytest.mark.django_db()
+def test_upvoted_filter_comment(client: Client, user: User, upvote_comment: PostVote) -> None:
     url = reverse("user_upvoted") + "?filter=comment"
     client.force_login(user)
     response = client.get(url)
-    comment.refresh_from_db()
-    upvote_comment.refresh_from_db()
     assert response.status_code == 200
-    assert comment in response.context["posts"]
+    assert upvote_comment.post in response.context["posts"]
+    assert len(response.context["posts"]) == 1
 
 
 @pytest.mark.django_db()
-def test_filter_all_post(client: Client, user: User, post: Post, upvote_post: PostVote) -> None:
-    url = reverse("user_upvoted") + "?filter="
+def test_upvoted_filter_comment_empty(client: Client, user: User, upvote_post: PostVote) -> None:
+    url = reverse("user_upvoted") + "?filter=comment"
     client.force_login(user)
     response = client.get(url)
-    post.refresh_from_db()
-    upvote_post.refresh_from_db()
     assert response.status_code == 200
-    assert post in response.context["posts"]
+    assert upvote_post.post not in response.context["posts"]
+    assert len(response.context["posts"]) == 0
 
 
 @pytest.mark.django_db()
-def test_filter_all_comment(client: Client, user: User, comment: Post, upvote_comment: PostVote) -> None:
+def test_upvoted_filter_all_post(client: Client, user: User, upvote_post: PostVote, upvote_comment: PostVote) -> None:
     url = reverse("user_upvoted") + "?filter="
     client.force_login(user)
     response = client.get(url)
-    comment.refresh_from_db()
-    upvote_comment.refresh_from_db()
     assert response.status_code == 200
-    assert comment in response.context["posts"]
+    assert upvote_post.post in response.context["posts"]
+    assert upvote_comment.post in response.context["posts"]
+    assert len(response.context["posts"]) == 2
