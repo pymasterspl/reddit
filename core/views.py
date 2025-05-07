61d7d70f5466b49d6afa733c825887d2f23d32f9
maksmondeo <maks.wazne@gmail.com>
2025-05-07 12:52:47 +0200

    Update community view to include images

--- core/views.py
+++ core/views.py
@@ -342,6 +342,12 @@ class CommunityDetailView(CommunityMixin, DetailView):
         context["moderators"] = CommunityMember.objects.filter(
             community=community, role=CommunityMember.MODERATOR
         ).select_related("user")
+        context["community_avatar_url"] = (
+            community.avatar.url if community.avatar else "/static/user_avatars/default.jpg"
+        )
+        context["community_background_url"] = (
+            community.background.url if community.background else "/static/user_avatars/default_banner.jpg"
+        )
         return context
 
     def post_add_moderator(self: "CommunityDetailView", request: "HttpRequest", *args: any, **kwargs: any) -> any:
